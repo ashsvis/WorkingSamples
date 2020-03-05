8c0682be1a6a32b72c5112990f1f027db6aa0abd
ashsvis <ashsvis@gmail.com>
2020-03-05 09:36:39 +0400

    Создание и настройка главной формы

--- ChartWorking/MainChartForm.cs
+++ ChartWorking/MainChartForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ChartWorking
 {
-    public partial class Form1 : Form
+    public partial class MainChartForm : Form
     {
-        public Form1()
+        public MainChartForm()
         {
             InitializeComponent();
         }
